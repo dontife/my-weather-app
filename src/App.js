1e7eb19cf430be8c1f4041556e9bb57bed9c21c9
dontife <dontife@ymail.com>
2022-10-20 12:57:11 -0400

    use react hooks to get the longitude and lattitude of my location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+  
   return (
     <div className="App">
     </div>
