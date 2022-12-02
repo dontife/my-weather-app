6550df69c94e57d6fa35be1de87ba0aaa40cbe42
dontife <dontife@ymail.com>
2022-12-01 23:35:21 -0500

    Get current location using latitude and longitude

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from "react";
 import './App.css';
-
-function App() {
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+export default function App() {
+  
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long]);
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
   
   return (
     <div className="App">
+      
     </div>
   );
 }
-
-export default App;
