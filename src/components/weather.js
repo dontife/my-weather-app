1b95e23d8d9678c1975da75a1b33850442955ac5
dontife <dontife@ymail.com>
2022-12-11 22:23:28 -0500

    add a refresh button

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './styles.css';
 import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+
+const refresh = () => {
+  window.location.reload();
+}
 
 const WeatherCard = ({weatherData}) => (
   <div className="main">
-      <p className="header">{weatherData.name}</p>
+
+      <div className="top">
+        <p className="header">{weatherData.name}</p>
+        <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+      </div>
       <div className="flex">
         <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
         <p className="description">{weatherData.weather[0].main}</p>
